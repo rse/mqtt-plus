7cef7f3af21ad4ac8d3fd38268106079e2ec76f9
Ralf S. Engelschall <rse@engelschall.com>
2026-02-06 21:47:02 +0100

    no need to check for Buffer

--- src/mqtt-plus-encode.ts
+++ src/mqtt-plus-encode.ts
@@ -57,7 +57,7 @@ export class EncodeTrait<T extends APISchema = APISchema> extends CodecTrait<T> 
     buf2arr (data: Uint8Array, type: new () => Int8Array): Int8Array
     buf2arr <T extends Buffer | Uint8Array | Int8Array>(data: Uint8Array, cons: new (...args: any[]) => T): T {
         let arr: T | undefined
-        if (typeof Buffer !== "undefined" && cons === (Buffer as unknown as new (...args: any[]) => T))
+        if (cons === (Buffer as unknown as new (...args: any[]) => T))
             arr = Buffer.from(data.buffer, data.byteOffset, data.byteLength) as T
         else if (cons === (Uint8Array as unknown as new (...args: any[]) => T))
             arr = data as T
