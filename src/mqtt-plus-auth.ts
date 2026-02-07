34944d8efc78f1a6a3d07e37079aabe60c3157e9
Ralf S. Engelschall <rse@engelschall.com>
2026-02-07 02:21:14 +0100

    increase iterations

--- src/mqtt-plus-auth.ts
+++ src/mqtt-plus-auth.ts
@@ -53,7 +53,7 @@ export class AuthTrait<T extends APISchema = APISchema> extends MetaTrait<T> {
         /*  use a derived key with minimum length of 32 for JWT HS256  */
         const pass = new TextEncoder().encode(credential)
         const salt = new TextEncoder().encode("mqtt-plus")
-        this._credential = pbkdf2.deriveKey(sha256.SHA256, pass, salt, 100000, 32)
+        this._credential = pbkdf2.deriveKey(sha256.SHA256, pass, salt, 600000, 32)
     }
 
     /*  issue client-side token on server-side  */
