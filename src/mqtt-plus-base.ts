975be0fe76488fd4c507c46cafa18025fe7c6ded
Ralf S. Engelschall <rse@engelschall.com>
2026-03-29 17:18:03 +0200

    perform a silent return

--- src/mqtt-plus-base.ts
+++ src/mqtt-plus-base.ts
@@ -110,7 +110,7 @@ export class BaseTrait<T extends APISchema = APISchema> extends TraceTrait<T> {
         return {
             destroy: async (): Promise<void> => {
                 if (!this.onRequest.has(key))
-                    throw new Error(`destroy: ${kind} "${name}" not registered`)
+                    return
                 await spool.unroll(false)?.catch((err: unknown) => {
                     const error = ensureError(err, `destroy: ${kind} "${name}" failed to cleanup`)
                     this.error(error)
